973faeba3d2b91df635de2621fbdda2b9f569a71
Guilherme Varela <guilhermevarela@hotmail.com>
2017-08-16 20:13:14 -0300

    logistic regression: ecommerce_data added

---
+++ ecommerce.py
@@ -0,0 +1,59 @@
+'''
+Created on Aug 16, 2017
+
+@author: Varela
+
+'''
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+from utils import get_iris, get_ecommerce, forward, cross_entropy, error_rate 
+
+def main():
+	user_action=3
+	X, T  = get_ecommerce(user_action=user_action)
+	# X, T  = np.shuffle(X,T)
+
+	N, D  = X.shape 
+	X 		= np.concatenate((np.ones((N,1)), X), axis=1, ) 
+	T = T.astype(np.int32)
+	X = X.astype(np.float32)
+	D+=1
+
+	# params
+	lr = 5e-4
+	max_iteration=1000
+	W  		= np.random.randn(D) / np.sqrt(D)
+	cost 	= []
+	error = [] 
+	for i in xrange(max_iteration):
+		Y = forward(W, X)
+		cost.append(cross_entropy(T,Y))
+		error.append(error_rate(T,Y))
+
+		W += lr*X.T.dot(T-Y)
+
+		if i % 5 == 0:
+			print "i=%d\tcost=%.3f\terror=%.3f" % (i,cost[-1],error[-1])
+
+	if i % 5 == 0:
+			print "i=%d\tcost=%.3f\terror=%.3f" % (i,cost[-1],error[-1])
+					
+	print "Final weight:", W 	
+
+	plt.title('logistic regression user_action=%d' % (user_action))
+	plt.xlabel('iterations')
+	plt.ylabel('cross entropy')
+	plt.plot(cost)
+	plt.show()
+
+	plt.title('logistic regression user_action=%d' % (user_action))
+	plt.xlabel('iterations')
+	plt.ylabel('error rate')
+	plt.plot(error)
+	plt.show()
+
+
+if __name__ == '__main__':
+	main()
+	
--- utils.py
+++ utils.py
@@ -8,6 +8,25 @@ Created on Aug 16, 2017
 import numpy as np 
 import pandas as pd 
 
+def error_rate(T,Y):
+	return np.mean(np.round(Y)!=T)
+
+# calculate the cross-entropy error
+def cross_entropy(T, Y):
+    E = 0
+    for i in xrange(T.shape[0]):
+        if T[i] == 1:
+            E -= np.log(Y[i])
+        else:
+            E -= np.log(1 - Y[i])
+    return E
+	
+def sigmoid(z):
+	return 1.0/(1.0 + np.exp(-z))
+
+def forward(W, X):
+	return sigmoid(X.dot(W))
+
 def get_iris(detect='Iris-setosa'):
 	X = []; Y =[]
 	if not( detect in ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']): 
@@ -16,4 +35,15 @@ def get_iris(detect='Iris-setosa'):
 	X=  df.values[:,:-1]
 	Y=  df.values[:,-1] == detect
 
+	return X, Y 
+
+def get_ecommerce(user_action=1):
+	X = []; Y =[]
+	if not( user_action in [1,2,3]): 
+		raise ValueError( 'value to user_action - %d invalid' % (user_action))
+
+	df = pd.read_csv('./datasets/ecommerce/ecommerce_data.csv', sep=',', header=0)
+	X=  df.values[:,:-1]
+	Y=  np.array(df.values[:,-1] == user_action, dtype=np.int32)
+	
 	return X, Y 
