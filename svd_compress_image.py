f6019f2e025977b02ac024a800cbcdd4f40cbf14
Guilherme Varela <guilhermevarela@hotmail.com>
2017-09-29 18:09:32 -0300

    svd: image compression updates

--- svd_compress_image.py
+++ svd_compress_image.py
@@ -9,9 +9,9 @@
 '''
 
 import numpy as np 
-
+import matplotlib.pyplot as plt 
 from utils import get_lena
-# from sklearn.decomposition import TruncatedSVD
+
 
 def svd_compress_gs(gsimg, r=3, verbose=True):
 	'''
@@ -20,19 +20,24 @@ def svd_compress_gs(gsimg, r=3, verbose=True):
 		gsimg .: gets a gray scale( 2D matrix where each pixel is represented by a 1 byte 0..255 ) and applies truncated SVD
 
 	'''	
+
 	u, s, v = np.linalg.svd(gsimg, full_matrices=True)
 
 	if verbose: 
-		print 'shapes: u:',u.shape,'\ts: ',s.shape,'\tv:\t',v.shape
-	# svd = TruncatedSVD(n_components=r)
-	# newimg = svd.fit_transform(gsimg)	
-	# explained_variance = svd.explained_variance_
-	# explained_variance_ratio = svd.explained_variance_ratio_	
+		# print 'shapes: U:',u.shape,'\tS: ',s.shape,'\tV:\t',v.shape
+		print 'explained_variance:', np.cumsum(s[:r]) / s.sum() 
+		print 'total_explained_variance:', s[:r].sum() / s.sum() 
+	
+	S = np.diag(s[:r])	
+	U = u[:,:r]
+	V = v[:r,:]
+	
+	newimg = U.dot(S.dot(V)).astype(np.uint8)
 
-	return newimg, explained_variance, explained_variance_ratio
+	return newimg, u, s, v
 
 
-def svd_compress_rgb(rgbimg, r=3):
+def svd_compress_rgb(rgbimg, r=3, verbose=True):
 	'''
 		reuses svd_compress_rgb
 		
@@ -41,31 +46,82 @@ def svd_compress_rgb(rgbimg, r=3):
 	'''	
 
 	norm = np.linalg.norm(rgbimg)
-	ch= ['r', 'g', 'b']
+	ch= ['red', 'green', 'blue']
 	channels={} 
-	channels_explained_variaces={}
-	channels_explained_variace_ratios={}
-	# channels_singular_values= {}
+	channels_u={}
+	channels_s={}
+	channels_v={}
 
 
+	stats_explained_variance={}
+	stats_explained_variance_ratio={}
+	
+	explained_variances= [] 
+	total_variances=	[]
+	for i, key in enumerate(ch):	
 
-	# red_img, red_explained_variance_ratio, red_singular_values = svd_compress_gs(rgbimg[:,:, 0], r=r)
-	# grn_img, grn_explained_variance_ratio, grn_singular_values = svd_compress_gs(rgbimg[:,:, 1], r=r)
-	# blu_img, blu_explained_variance_ratio, blu_singular_values = svd_compress_gs(rgbimg[:,:, 2], r=r)
-	for i, key in enumerate(ch):
-		img, explained_variance, explained_variance_ratio = svd_compress_gs(rgbimg[:,:, i], r=r)
 
-		# print ch[i]
-		# print img[0,:10]
-		# print 'explained_variance:', explained_variance
-		# print 'explained_variance_ratio:', explained_variance_ratio
+		newimg, u, s, v = svd_compress_gs(rgbimg[:,:, i], r=r)	
 
-		# channels[i] = img 
-		# channels_explained_variaces[i]= explained_variance
-		# channels_explained_variace_ratios[i]= explained_variance_ratio
+		channels[i] = newimg 
+		channels_u[i]= u
+		channels_s[i]= s
+		channels_v[i]= v
+
+		stats_explained_variance[i] = np.cumsum(s[:r]) 
+		stats_explained_variance_ratio[i] = np.cumsum(s[:r]) / np.sum(s)
+
+		explained_variances.append(stats_explained_variance[i][-1])
+		total_variances.append(np.sum(s))
 		
+	
 
 
-if __name__ == '__main__':
+	newimg = np.dstack(tuple(channels.values()))
+
+	ratio = sum(explained_variances) / sum(total_variances)
+
+	if verbose:
+		plt.title('Reconstructed image (explained=%.2f)' % (ratio))
+		plt.imshow(newimg)
+		# plt.show()
+	
+	return newimg, ratio 		
+
+def main():
 	img = get_lena()
-	svd_compress_rgb(img, r=3)
+
+	min_size = min(img.shape[0:2])
+	R = [0.05, 0.10 , 0.25, 0.50]
+	dims= [] 
+	images=[] 
+	ratios=[] 
+	for r in R:
+		d = int(min_size*r)
+		newimg, ratio= svd_compress_rgb(img, r=d, verbose=False)
+
+		dims.append(d)
+		images.append(newimg)
+		ratios.append(ratio)
+
+	# Four axes, returned as a 2-d array
+	f, axarr = plt.subplots(2, 2)
+	axarr[0, 0].imshow(images[0])
+	axarr[0, 0].set_title('dims=%d, ratio=%.2f' % (dims[0], ratios[0]) )
+	axarr[0, 1].imshow(images[1])
+	axarr[0, 1].set_title('dims=%d, ratio=%.2f' % (dims[1], ratios[1]) )
+	axarr[1, 0].imshow(images[2])
+	axarr[1, 0].set_title('dims=%d, ratio=%.2f' % (dims[2], ratios[2]) )
+	axarr[1, 1].imshow(images[3])
+	axarr[1, 1].set_title('dims=%d, ratio=%.2f' % (dims[3], ratios[3]) )
+	
+	# Fine-tune figure; hide x ticks for top plots and y ticks for right plots
+	plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
+	plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
+	plt.show()
+	# plt.imshow(images)
+
+	
+if __name__ == '__main__':
+	main()
+
